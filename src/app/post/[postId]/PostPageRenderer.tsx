74d89d061f02acf552d10346cd2fc3c85bcf5481
ezpieco <ezpie.co@gmail.com>
2025-07-22 22:36:15 +0530

    refractor: move functionality of blog page to backend API

--- src/app/post/[postId]/PostPageRenderer.tsx
+++ src/app/post/[postId]/PostPageRenderer.tsx
@@ -9,45 +9,47 @@ import SideBar from "@/components/Banners/SideBar";
 import "@/styles/PostPage/PostPage.css";
 
 interface Props {
-  post: postWithAuthor;
+  post: postWithAuthor | undefined;
   loggedInUsername: string | undefined;
 }
 
 export default function PostRenderer({ post, loggedInUsername }: Props) {
 
-  return (
-    <>
-      <nav className="sidebar">
-        <SideBar
-          loggedInUsername={String(loggedInUsername)}
-        />
-      </nav>
-      <section className="post p-5">
-        <div className="mb-10">
-          <h1 className="font-anonymous mb-8 text-2xl font-extrabold">
-            {post.title}
-          </h1>
-          <div className="post-content">
-            {post.content && <Formatter postContent={post.content} />}
+  if (post) {
+    return (
+      <>
+        <nav className="sidebar">
+          <SideBar
+            loggedInUsername={String(loggedInUsername)}
+          />
+        </nav>
+        <section className="post p-5">
+          <div className="mb-10">
+            <h1 className="font-anonymous mb-8 text-2xl font-extrabold">
+              {post.title}
+            </h1>
+            <div className="post-content">
+              {post.content && <Formatter postContent={post.content} />}
+            </div>
           </div>
-        </div>
-        <div>
-          <Like post={post} />
-        </div>
-      </section>
-      <section className="w-80">
-        <p>
-          <Link
-            href={`/user/${post.author?.username}`}
-            className="font-anonymous hover:underline"
-          >
-            {post.author?.username}
-          </Link>
-        </p>
-        <p className="font-inter text-sm text-neural-300">
-          {post.author?.description}
-        </p>
-      </section>
-    </>
-  )
+          <div>
+            <Like post={post} />
+          </div>
+        </section>
+        <section className="w-80">
+          <p>
+            <Link
+              href={`/user/${post.author?.username}`}
+              className="font-anonymous hover:underline"
+            >
+              {post.author?.username}
+            </Link>
+          </p>
+          <p className="font-inter text-sm text-neural-300">
+            {post.author?.description}
+          </p>
+        </section>
+      </>
+    )
+  }
 }
--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -1,8 +1,8 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+"use client";
 
 import CommentSection from "@/components/Post/CommentSection";
 import PostRenderer from "@/app/post/[postId]/PostPageRenderer";
+import { useEffect, useState } from "react";
 
 // Tell's vercel that this is a dynamic function
 export const dynamic = "force-dynamic";
@@ -14,54 +14,36 @@ export const dynamic = "force-dynamic";
  *
  * @returns JSX.Element
  */
-export default async function Page({ params }: { params: { postId: string } }) {
-  const supabase = createServerComponentClient<Database>({ cookies });
+export default function Page({ params }: { params: { postId: string } }) {
+  const [postInfo, setPostInfo] = useState<postWithAuthor>();
+  const [username, setUsername] = useState();
 
-  // get the current session of the user
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  // Get the information about the post which is selected
-  const { data } = await supabase
-    .from("Blogs")
-    .select("*, author: profiles(*), Likes(user_id)")
-    .filter("id", "eq", params.postId);
-
-  // Destructure data and add items - user_liked_post[boolean], likes[length of likes the post has]
-  const postInfo =
-    data?.map((post) => ({
-      ...post,
-      user_liked_post: post.Likes.some(
-        (like) => like.user_id === session?.user.id
-      ),
-      likes: post.Likes.length,
-    })) ?? [];
-
-  // used by the PostRenderer component
-  const { data: currentLoggedInUsernameArray } = await supabase
-    .from("profiles")
-    .select("username")
-    .eq("id", String(session?.user.id))
-    .single();
-
-  // the returned data is an object - {username: name} 
-  // For example - {username: "tester1"}
-  const currentLoggedInUsername = currentLoggedInUsernameArray?.username;
-
-  if (postInfo) {
-    const post = postInfo[0];
-
-    /* eslint-disable max-len */
-    return (
-      <div>
-        <section className="md:flex justify-between md:mx-2 mx-1 my-4 pb-10 post-divider">
-          <PostRenderer post={post} loggedInUsername={currentLoggedInUsername} />
-        </section>
-        <section className="ml-[17rem]">
-          <CommentSection post={params.postId} />
-        </section>
-      </div>
-    );
+  const fetchPost = async () => {
+    const res = await fetch(`/api/posts/${params.postId}`)
+    const data = await res.json();
+    setPostInfo(data.post)
   }
+
+  const fetchUser = async () => {
+    const res = await fetch("/api/users/me")
+    const user = await res.json();
+    setUsername(user.username)
+  }
+
+  useEffect(() => {
+    fetchUser();
+    fetchPost();
+  }, [])
+
+  /* eslint-disable max-len */
+  return (
+    <div>
+      <section className="md:flex justify-between md:mx-2 mx-1 my-4 pb-10 post-divider">
+        <PostRenderer post={postInfo} loggedInUsername={username} />
+      </section>
+      <section className="ml-[17rem]">
+        <CommentSection postId={params.postId} />
+      </section>
+    </div>
+  );
 }
